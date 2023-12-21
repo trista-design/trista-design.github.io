8ebbf78c13207ad427777b7e64bb3edd7cd2d2a4
Trista <tristahung.design@gmail.com>
2023-12-21 22:19:54 +0800

    fixBug

--- js/navChange.js
+++ js/navChange.js
@@ -2,7 +2,6 @@
 const menu = document.querySelector(".nav-menu");
 const background = document.querySelector(".nav-background");
 const pageBTN = document.querySelectorAll(".page-btn");
-console.log(pageBTN);
 
 window.onscroll = function () {
   scrollChange();
@@ -10,13 +9,13 @@ window.onscroll = function () {
 
 function scrollChange() {
   if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
-    background.style = "animation:show 0.5s ease ; opacity:1";
+    background.style = "opacity:1";
     menu.style = "padding: 0 24px 0 32px";
   } else if (window.innerWidth <= 490) {
-    background.style = "animation:close 0.5s ease ; opacity:0";
+    background.style = "opacity:0";
     menu.style = "padding: 0 0 0 24px";
   } else {
-    background.style = "animation:close 0.5s ease ; opacity:0";
+    background.style = "opacity:0";
     menu.style = "padding: 0";
   }
 }
@@ -29,7 +28,7 @@ pageBTN.forEach((btn) => {
     ) {
       this.style.backgroundColor = "var(--white)";
     } else if (window.innerWidth <= 768) {
-      this.style.backgroundColor = "var(--black-600)";
+      this.style.backgroundColor = "var(--white)";
     } else {
       this.style.backgroundColor = "var(--black-200)";
     }
@@ -42,14 +41,15 @@ pageBTN.forEach((btn) => {
 // menu - burger
 const burger = document.querySelector("#menu-slider");
 const mask = document.querySelector(".menu-mask");
+const navLeftItem = document.querySelector(".nav-menu-left");
 let clickCount = 0;
 
 burger.addEventListener("click", (e) => {
   // 打開burger選單時，無法向下滑動(y)
   if (burger.checked) {
-    document.documentElement.style.overflowY = "hidden";
+    navLeftItem.style.opacity = "0";
   } else {
-    document.documentElement.style.overflowY = "auto";
+    navLeftItem.style.opacity = "1";
   }
   clickCount++;
   // 根據點擊次數更換mask動畫
@@ -65,10 +65,10 @@ burger.addEventListener("click", (e) => {
 // 頁面連結
 const workPage = document.querySelector("#work-page");
 workPage.addEventListener("click", () => {
-  window.location.href = "/index.html";
+  window.location.href = "/";
 });
 
 const aboutPage = document.querySelector("#about-page");
 aboutPage.addEventListener("click", () => {
-  window.location.href = "/about.html";
+  window.location.href = "/about";
 });
